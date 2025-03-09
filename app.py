eac5148b8a0fd1af392dc607746e9d98f24244c3
pranchan <pranchan@iu.edu>
2025-03-08 19:17:22 -0500

    Defined some of the key user inputs like experience, level, weight, wingspan, and etc.. Also established API homepage and predict variable for user input

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify
+
+app=Flask(__name__) #Instance of flask class. The Flask class is called to create an application object. The __name__ is the name of the current module
+
+@app.route("/" ,methods=["GET"]) # @app.route is the decorator, "/" this meanns its the homepage of the API. methods=["GET"] this route only allows get requests
+def home(): # Function runs when someone visits the /
+    return jsonify({"message":"Welcome to 1v1 World!"}) # Converts the dictionary into a JSON response.
+
+@app.route("/predict", methods=["POST"]) #"/predict" is a new API endpoint. methods=["POST"] only takes post requests
+def predict(): # Will run whever a user inputs stats 
+    data=request.get_json() # Extracts Json data sent in the request
+
+    experience= data.get("experience",0)
+    level= data.get("level","recreational")
+    height= data.get("height",0)
+    weight= data.get("weight",0)
+    wingspan= data.get("wingspan",0)
+    shooting= data.get("shooting",0)
+    dribbling= data.get("dribbling",0)
+    speed= data.get("speed",0)
+    agility= data.get("agility",0)
+
+
+
